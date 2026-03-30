348e0c135a05d5a1117e0c06972b01b40fdd5b7b
shabslaw <shabslaw@gmail.com>
2026-03-30 03:06:08 -0700

    Create API to get a single order

--- server.js
+++ server.js
@@ -189,6 +189,35 @@ app.get('/orders', async (req, res) => {
     res.json(orders);
 });
 
+// API route to get a single order by its ID
+app.get('/orders/:orderId', async (req, res) => {
+    const { orderId } = req.params;
+    const expand = req.query.expand;
+
+    let order = await Order.findByPk(orderId);
+    if (!order) {
+        return res.status(404).json({ error: 'Order Not found' });
+    }
+
+    if (expand === 'products') {
+        const products = await Promise.all(order.products.map(async (product) => {
+            const productDetails = await Product.findByPk(product.productId);
+
+            return {
+                ...product,
+                product: productDetails
+            };
+        }));
+
+        order = {
+            ...order.toJSON(),
+            products
+        };
+    }
+
+    res.json(order);
+});
+
 
 // API toute to create an order
 app.post('/orders', async (req, res) => {
