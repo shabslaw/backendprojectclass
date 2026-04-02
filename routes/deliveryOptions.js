81a4aabcc110f4972cb833e3f87e2d88f498d8b6
shabslaw <shabslaw@gmail.com>
2026-04-02 04:23:08 -0700

    Add expand=estimatedDeliveryTime to the /delivery-options API

--- routes/deliveryOptions.js
+++ routes/deliveryOptions.js
@@ -3,9 +3,23 @@ import { DeliveryOptions } from "../models/DeliveryOptions.js";
 
 const router = express.Router()
 
-router.get('/', async (requestAnimationFrame, res) => {
+router.get('/', async (req, res) => {
+    const expand = req.query.expand;
     const deliveryOptions = await DeliveryOptions.findAll();
-    res.json(deliveryOptions);
+
+    let response = deliveryOptions;
+
+    if (expand === 'estimatedDeliveryTime') {
+        response = deliveryOptions.map(option => {
+            const deliveryTimeMs = Date.now() + option.deliveryDays * 24 * 60 * 60 * 1000;
+            return {
+                ...option.toJSON(),
+                estimatedDeliveryTime: deliveryTimeMs
+            };
+        });
+    }
+
+    res.json(response);
 });
 
 export default router;
